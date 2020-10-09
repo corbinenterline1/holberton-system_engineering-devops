979f59888e5040405f5cf3e1ce2aa93423d2d35d
Corbin Enterline <corbinenterline1@gmail.com>
2020-10-09 01:31:55 +0000

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # fixes number of simultaneous requests for NGINX
 exec { 'fix':
   path    => ['/usr/bin/', '/bin/', '/sbin', '/usr/sbin/'],
-  command => "sed -i '5c ULIMIT=\"-n 1000\"' && sudo service nginx restart"
+  command => "sed -i '5c ULIMIT=\"-n 3000\"' && sudo service nginx restart"
 }
 exec { 'restart':
   provider => 'shell',
