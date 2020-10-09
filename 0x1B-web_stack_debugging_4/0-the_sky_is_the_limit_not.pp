9ee7d077324c7c775558042d8873d68a0fa9efc3
Corbin Enterline <corbinenterline1@gmail.com>
2020-10-09 01:16:53 +0000

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # fixes number of simultaneous requests for NGINX
 exec { 'fix':
-  path    => ['/usr/bin/', '/bin/'],
-  command => 'echo ULIMIT=\" -n 4096\" >> /etc/default/nginx',
+  path    => ['/usr/bin/', '/bin/', '/sbin', '/usr/sbin/'],
+  command => 'echo ULIMIT=\" -n 1000\" >> /etc/default/nginx',
 }
 exec { 'restart':
   provider => 'shell',
